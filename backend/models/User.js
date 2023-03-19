aa22e74054ef6e4c56ae2c9c5c333b9c61814f42
Kelly <kelly.a.artola@ricealumni.net>
2023-03-19 15:56:45 -0500

    virtual password field

--- backend/models/User.js
+++ backend/models/User.js
@@ -54,5 +54,37 @@ const userSchema = new schema(
   { timestamp: true }
 );
 
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
+
 const User = mongoose.model("User", userSchema);
 module.exports = User;
