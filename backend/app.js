33d3c598ec471afed2aa3633fe5f45fe91d0247b
Kelly <kelly.a.artola@ricealumni.net>
2023-03-18 19:15:16 -0500

    require morgan

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 require("dotenv/config");
 
 const api = process.env.API;
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
