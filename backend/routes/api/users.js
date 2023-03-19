fb93f1028777344781aba0bbf91e7a1bd002dcd1
Kelly <kelly.a.artola@ricealumni.net>
2023-03-19 15:39:39 -0500

    register validation

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/User");
+
+const { runValidation } = require("../../validation");
+const { validateRegisterInput } = require("../../validation/register");
 
 router.get("/test", (req, res) => {
   res.json({ msg: "This is the user route" });
