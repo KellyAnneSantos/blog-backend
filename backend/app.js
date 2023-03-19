05928664f5230f87473eccd4ca0cdc6abdb4b8d1
Kelly <kelly.a.artola@ricealumni.net>
2023-03-18 20:36:57 -0500

    express router

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
 
 // require("dotenv/config");
 
@@ -11,16 +12,18 @@ const db = require("./config/keys").mongoURI;
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-const port = process.env.PORT || 3000;
-
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("connected to mongoDB"))
   .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
